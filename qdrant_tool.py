4383c8ca931b309df2cd6cb1a7f6ca507fbe0501
lifeofsoumya <mondalsoumya2001@gmail.com>
2025-05-07 16:03:09 +0530

    chore: kb name, thread from .env

--- qdrant_tool.py
+++ qdrant_tool.py
@@ -32,7 +32,7 @@ def qdrant_tool(question: str, top_k:int) -> str:
     embedding = response.data[0].embedding
 
     search_result = connection.query_points(
-        collection_name="1finance_kb_department",
+        collection_name=os.getenv("QDRANT_COLLECTION_NAME"),
         query=embedding,
         limit=top_k,
         with_payload=True
